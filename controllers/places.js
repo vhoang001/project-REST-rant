bd52519424adc8bf4d2df8da6e229f221d1ebe1c
Van Hoang <hoangduykhanhvan@gmail.com>
2024-03-07 21:23:15 -0800

    new places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,11 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
+
+router.get('/', (req, res) => {
+    res.render('places/index', { places })
+})
+
+// More code ...
 
 router.get('/new', (req, res) => {
     res.render('places/new')
@@ -27,6 +34,17 @@ module.exports = router
 // POST /places
 router.post('/', (req, res) => {
     console.log(req.body)
-    res.send('POST /places')
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
   })
   
