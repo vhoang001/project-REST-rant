f030b0ffe9c124f3fbda76d9af9a10ee4517947a
Van Hoang <hoangduykhanhvan@gmail.com>
2024-03-21 19:40:37 -0700

    Our First Full-Stack App (be-9-rest-rant-part-10)

--- controllers/places.js
+++ controllers/places.js
@@ -127,11 +127,6 @@ router.post('/', (req, res) => {
   })
 })
 
-
-
-
-
-
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
@@ -139,11 +134,35 @@ router.get('/new', (req, res) => {
 //Fix the Show Page and Links
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
+  .populate('comments')
   .then(place => {
+      console.log(place.comments)
       res.render('places/show', { place })
   })
   .catch(err => {
       console.log('err', err)
+      res.render('error404')
+  })
+})
+
+//Create a Comment
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
       res.render('error404')
   })
 })
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,47 +1,62 @@
 const React = require('react')
 const Def = require('../default')
 
-function new_form (data) {
-  let message = 'Hello'                 
-    if (data.message) {
-      message = (
-        <h4 className="alert-danger">
-          {data.message}
-        </h4>
-      )
+function show (data) {
+    let comments = (
+      <h3 className="inactive">
+        No comments yet!
+      </h3>
+    )
+    if (data.place.comments.length) {
+      comments = data.place.comments.map(c => {
+        return (
+          <div className="border">
+            <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+            <h4>{c.content}</h4>
+            <h3>
+              <stong>- {c.author}</stong>
+            </h3>
+            <h4>Rating: {c.stars}</h4>
+          </div>
+        )
+      })
     }
     return (
         <Def>
-            <main>
-                <h1>Add a New Place</h1>
-                {message}                 
-            
-                <div className="row">
-                    
-                        <img src={data.place.pic} alt={data.place.name} />
+          <main>
+            <div className="row">
+                <img src={data.place.pic} alt={data.place.name} />
 
-                        <h3>
-                        Located in {data.place.city}, {data.place.state}
-                        </h3>
+                <h3>
+                Located in {data.place.city}, {data.place.state}
+                </h3>
 
-                        <h2>
-                            Description
-                        </h2>
+                <h2>
+                    Description
+                </h2>
 
-                        <h3>
-                            {data.place.showEstablished()}
-                        </h3>
+                <h3>
+                    {data.place.showEstablished()}
+                </h3>
 
-                        <h4>
-                            Serving {data.place.cuisines}
-                        </h4>
-                </div>
-            </main>
+                <h4>
+                    Serving {data.place.cuisines}
+                </h4>
+            </div>
+            <hr />
+            <h2>Comments</h2>
+            {comments}
+          </main>
         </Def>
     )
 }
 
-module.exports = new_form
+module.exports = show
+
+
+
+                    
+
 
 
 
